3beefe12d0bb241886e12d4282df0e1c9f9e408c
Matheus <matheuzim88@hotmail.com>
2025-03-12 20:35:58 -0300

    criação funções novas

--- 1º Bimestre/Desenvolvimento Web 2 (Ely)/nodeJS/API Rest Node Js/public/usuario.js
+++ 1º Bimestre/Desenvolvimento Web 2 (Ely)/nodeJS/API Rest Node Js/public/usuario.js
@@ -1,3 +1,56 @@
+const modalcadastro = new bootstrap.Modal(document.getElementById('modalcadastro'))
+
+function alterar(id){
+
+}
+
+function excluir(id){
+    fetch("http://127.0.0.1:4444/usuario/"+idusuario,
+        {
+            method: "DELETE"
+        }
+    ).then(function(){
+        //recarrega a lista
+        listar();
+    });
+}
+
+function novo(){
+    document.getElementById("nome").value="";
+    document.getElementById("telefone").value="";
+    document.getElementById("email").value="";
+    modalcadastro.show();
+}
+
+function salvar(){
+    let vnome = document.getElementById("nome").value;
+    let vtelefone = document.getElementById("telefone").value;
+    let vemail = document.getElementById("email").value;
+
+    let usuario = {
+        nome: vnome,
+        telefone: vtelefone,
+        email: vemail,
+        senha: ""
+    }
+
+    fetch("http://127.0.0.1:4444/usuario",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(usuario)
+        }
+    ).then(function(){
+        //recarrega a lista
+        listar();
+        //esconde o modal
+        modalcadastro.hide();
+    })
+}
+
+
 function listar(){
     const lista = document.getElementById("lista");
     lista.innerHTML = "<tr><td colspan='5'>Carregando...</td></tr>";
@@ -15,7 +68,10 @@ function mostrar(dados){
         + "<td>" + dados[i].nome + "</td>"
         + "<td>" + dados[i].telefone + "</td>"
         + "<td>" + dados[i].email + "</td>"
-        + "<td></td>"
+        + "<td>"
+        + "<button type='button' class='btn btn-primary' onclick='alterar("+dados[i].idusuario+")'>A</button>"
+        + "<button type='button' class='btn btn-danger' onclick='excluir("+dados[i].idusuario+")'>X</button>"
+        + "</td>"
         + "</tr>";
     }
 
