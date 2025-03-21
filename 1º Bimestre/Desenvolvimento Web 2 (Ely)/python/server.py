37eb8489032c4b1e19b7fc740b9998fb4696d404
Matheus <matheuzim88@hotmail.com>
2025-03-20 21:45:47 -0300

    criação python

--- 1º Bimestre/Desenvolvimento Web 2 (Ely)/python/server.py
+++ 1º Bimestre/Desenvolvimento Web 2 (Ely)/python/server.py
@@ -1,2 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, send_from_directory
+from usuarios import usuario_bp
+import os
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static'
+            )
+app.register_blueprint(usuario_bp)
+
+@app.route('/')
+def home():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'index.html')
+
+if __name__ == '__main__':
+    app.run()
--- 1º Bimestre/Desenvolvimento Web 2 (Ely)/python/usuarios.py
+++ 1º Bimestre/Desenvolvimento Web 2 (Ely)/python/usuarios.py
@@ -1,10 +1,12 @@
 import pymysql
-from server import app
 from db_config import connect_db
 from flask import jsonify
-from flask import flash, request
+from flask import flash, request, Blueprint
 
-@app.route('/usuarios')
+usuario_bp = Blueprint("usuario", __name__)
+
+
+@usuario_bp.route('/usuario')
 def usuarios():
     try:
         conn = connect_db()
@@ -20,5 +22,71 @@ def usuarios():
         cursor.close()
         conn.close()
 
-if __name__ == "__main__":
-    app.run()
+
+@usuario_bp.route('/usuario/<id>')
+def usuariobyid(id):
+    try:
+        conn = connect_db()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT * FROM usuario WHERE idusuario = %s", (id))
+        rows = cursor.fetchall()
+        resp = jsonify(rows[0])
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@usuario_bp.route('/usuario', methods=["POST"])
+def usuarionovo():
+    try:
+        conn = connect_db()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+        #pegar os dados do JSON
+        usuario = request.json
+        nome = usuario['nome']
+        email = usuario['email']
+        senha = usuario['senha']
+        telefone = usuario['telefone']
+        cursor.execute("INSERT INTO usuario (nome, email, senha, telefone) VALUES (%s, %s, %s, %s)", (nome, email, senha, telefone))
+
+        conn.commit()
+        resp = jsonify({"message": "inserido"})
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+#para casa: implemente os métodos PUT e DELETE
+@usuario_bp.route('/usuario', methods=["POST"])
+def usuarionovo():
+    try:
+        conn = connect_db()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+        #pegar os dados do JSON
+        usuario = request.json
+        nome = usuario['nome']
+        email = usuario['email']
+        senha = usuario['senha']
+        telefone = usuario['telefone']
+        cursor.execute("INSERT INTO usuario (nome, email, senha, telefone) VALUES (%s, %s, %s, %s)", (nome, email, senha, telefone))
+
+        conn.commit()
+        resp = jsonify({"message": "inserido"})
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
